3e4ac0f534865eccc42e10bd3ca8bd3b580ea333
Luka <luka.polanic@gmail.com>
2023-02-18 20:00:21 +0100

    Commit 1 Database

---
+++ application.py
@@ -0,0 +1,30 @@
+from flask import Flask, request
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+
+    def __repr__(self):
+        return f"{self.name} ({self.email})"
+
+
+class Group(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(120))
+
+    def __repr__(self):
+        return f"{self.name} - {self.description}"
+
+
+@app.route('/')
+def index():
+    return 'Hello!'
