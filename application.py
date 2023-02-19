e12ea4fbe9f94b6807e1ff65410c4c4095f5fa57
Luka <luka.polanic@gmail.com>
2023-02-19 19:23:44 +0100

    Commit 2 - users

--- application.py
+++ application.py
@@ -28,3 +28,44 @@ class Group(db.Model):
 @app.route('/')
 def index():
     return 'Hello!'
+
+@app.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    output = []
+    for user in users:
+        user_data = {'id': user.id, 'name': user.name, 'email': user.email, 'group_id': user.group_id}
+        output.append(user_data)
+    return {"users": output}
+
+@app.route('/users/<id>', methods=['GET'])
+def get_user(id):
+    user = User.query.get_or_404(id)
+    return {"id": user.id, "name": user.name, "email": user.email, "group_id": user.group_id}
+
+@app.route('/users', methods=['POST'])
+def add_user():
+    user = User(name=request.json['name'],
+                email=request.json['email'],
+                group_id=request.json.get('group_id'))
+    db.session.add(user)
+    db.session.commit()
+    return {'id': user.id}
+
+
+@app.route('/users/<id>', methods=['PUT'])
+def update_user(id):
+    user = User.query.get(id)
+    user.name = request.json.get('name', user.name)
+    user.email = request.json.get('email', user.email)
+    user.group_id = request.json.get('group_id', user.group_id)
+    db.session.commit()
+    return {"message": "updated"}
+
+
+@app.route('/users/<id>', methods=['DELETE'])
+def delete_user(id):
+    user = User.query.get(id)
+    db.session.delete(user)
+    db.session.commit()
+    return {"message": "deleted"}
