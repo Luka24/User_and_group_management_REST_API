99a7d603156f3996f55655ecf560a23983d74497
Luka <luka.polanic@gmail.com>
2023-02-22 19:36:50 +0100

    Commit 4 tests, get_groups() improved, database update

--- application.py
+++ application.py
@@ -6,42 +6,85 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 db = SQLAlchemy(app)
 
-class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(80), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
-
-    def __repr__(self):
-        return f"{self.name} ({self.email})"
-
-
 class Group(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
-    description = db.Column(db.String(120))
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+    name = db.Column(db.String(120), nullable=False)
+    group_id = db.Column(db.Integer, db.ForeignKey('group.id'))
+    group = db.relationship('Group', backref='users')
 
     def __repr__(self):
-        return f"{self.name} - {self.description}"
-
+        return f"{self.email} - {self.name}"
 
 @app.route('/')
 def index():
     return 'Hello!'
 
-@app.route('/users', methods=['GET'])
+@app.route('/groups')
+def get_groups():
+    groups = Group.query.all()
+    output = []
+    for group in groups:
+        group_data = {'id': group.id, 'name': group.name}
+        user_data = []
+        for user in group.users:
+            user_data.append({'email': user.email, 'name': user.name})
+        group_data['users'] = user_data
+        output.append(group_data)
+    return {"groups": output}
+
+
+@app.route('/groups/<id>')
+def get_group(id):
+    group = Group.query.get_or_404(id)
+    group_data = {'id': group.id, 'name': group.name}
+    user_data = []
+    for user in group.users:
+        user_data.append({'email': user.email, 'name': user.name})
+    group_data['users'] = user_data
+    return group_data
+
+@app.route('/groups', methods=['POST'])
+def add_group():
+    group = Group(name=request.json['name'])
+    db.session.add(group)
+    db.session.commit()
+    return {'id': group.id}
+
+@app.route('/groups/<id>', methods=['PUT'])
+def update_group(id):
+    group = Group.query.get(id)
+    group.name = request.json.get('name', group.name)
+    db.session.commit()
+    return {"message": "updated"}
+
+@app.route('/groups/<id>', methods=['DELETE'])
+def delete_group(id):
+    group = Group.query.get(id)
+    db.session.delete(group)
+    db.session.commit()
+    return {"message": "deleted"}
+
+@app.route('/users')
 def get_users():
     users = User.query.all()
     output = []
     for user in users:
-        user_data = {'id': user.id, 'name': user.name, 'email': user.email, 'group_id': user.group_id}
+        user_data = {'email': user.email, 'password': user.password, 'name': user.name, 'group_id': user.group_id}
         output.append(user_data)
     return {"users": output}
 
-@app.route('/users/<id>', methods=['GET'])
+
+@app.route('/users/<id>')
 def get_user(id):
     user = User.query.get_or_404(id)
-    return {"id": user.id, "name": user.name, "email": user.email, "group_id": user.group_id}
+    return {"name": user.name, "email": user.email, "group": user.group.name if user.group else None}
+
 
 @app.route('/users', methods=['POST'])
 def add_user():
@@ -50,14 +93,13 @@ def add_user():
                 group_id=request.json.get('group_id'))
     db.session.add(user)
     db.session.commit()
-    return {'id': user.id}
-
 
 @app.route('/users/<id>', methods=['PUT'])
 def update_user(id):
     user = User.query.get(id)
-    user.name = request.json.get('name', user.name)
     user.email = request.json.get('email', user.email)
+    user.password = request.json.get('password', user.password)
+    user.name = request.json.get('name', user.name)
     user.group_id = request.json.get('group_id', user.group_id)
     db.session.commit()
     return {"message": "updated"}
@@ -70,43 +112,5 @@ def delete_user(id):
     db.session.commit()
     return {"message": "deleted"}
 
-@app.route('/groups', methods=['GET'])
-def get_groups():
-    groups = Group.query.all()
-    output = []
-    for group in groups:
-        group_data = {'id': group.id, 'name': group.name, 'description': group.description}
-        output.append(group_data)
-    return {"groups": output}
-
-
-@app.route('/groups/<id>', methods=['GET'])
-def get_group(id):
-    group = Group.query.get_or_404(id)
-    return dict(id=group.id, name=group.name)
-
-@app.route('/groups', methods=['POST'])
-def add_group():
-    group = Group(name=request.json['name'],
-    description=request.json.get('description'))
-    db.session.add(group)
-    db.session.commit()
-    return {'id': group.id}
-
-@app.route('/groups/<id>', methods=['PUT'])
-def update_group(id):
-    group = Group.query.get(id)
-    group.name = request.json.get('name', group.name)
-    group.description = request.json.get('description', group.description)
-    db.session.commit()
-    return {"message": "updated"}
-
-@app.route('/groups/<id>', methods=['DELETE'])
-def delete_group(id):
-    group = Group.query.get(id)
-    db.session.delete(group)
-    db.session.commit()
-    return {"message": "deleted"}
-
-#if name == 'main':
-#app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ templates/Tests.py
@@ -0,0 +1,179 @@
+import json
+import unittest
+
+from app import app, db, Group, User
+
+
+class AppTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """Called before each test."""
+        self.app = app.test_client()
+        # create a new in-memory database for testing
+        app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///:memory:'
+        db.create_all()
+
+    def tearDown(self):
+        """Called after each test."""
+        db.session.remove()
+        db.drop_all()
+
+    def test_add_group(self):
+        """Test adding a new group."""
+        data = {'name': 'Test Group'}
+        response = self.app.post('/groups', json=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('id', response.json)
+
+        group_id = response.json['id']
+        group = Group.query.get(group_id)
+        self.assertIsNotNone(group)
+        self.assertEqual(group.name, data['name'])
+
+    def test_get_groups(self):
+        """Test getting a list of all groups."""
+        # Add some groups to the database
+        Group(name='Group 1')
+        Group(name='Group 2')
+        db.session.commit()
+
+        response = self.app.get('/groups')
+        self.assertEqual(response.status_code, 200)
+        groups = response.json['groups']
+        self.assertEqual(len(groups), 2)
+
+    def test_get_group(self):
+        """Test getting a single group by ID."""
+        group = Group(name='Test Group')
+        db.session.add(group)
+        db.session.commit()
+
+        response = self.app.get(f'/groups/{group.id}')
+        self.assertEqual(response.status_code, 200)
+        group_data = response.json
+        self.assertEqual(group_data['id'], group.id)
+        self.assertEqual(group_data['name'], group.name)
+
+    def test_update_group(self):
+        """Test updating an existing group."""
+        group = Group(name='Test Group')
+        db.session.add(group)
+        db.session.commit()
+
+        data = {'name': 'New Group Name'}
+        response = self.app.put(f'/groups/{group.id}', json=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {'message': 'updated'})
+
+        updated_group = Group.query.get(group.id)
+        self.assertEqual(updated_group.name, data['name'])
+
+    def test_delete_group(self):
+        """Test deleting an existing group."""
+        group = Group(name='Test Group')
+        db.session.add(group)
+        db.session.commit()
+
+        response = self.app.delete(f'/groups/{group.id}')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {'message': 'deleted'})
+
+        deleted_group = Group.query.get(group.id)
+        self.assertIsNone(deleted_group)
+
+    def test_add_user(self):
+        """Test adding a new user."""
+        data = {
+            'email': 'test@example.com',
+            'password': 'password123',
+            'name': 'Test User',
+            'group_id': None,
+        }
+        response = self.app.post('/users', json=data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('id', response.json)
+
+        user_id = response.json['id']
+        user = User.query.get(user_id)
+        self.assertIsNotNone(user)
+        self.assertEqual(user.email, data['email'])
+        self.assertEqual(user.password, data['password'])
+        self.assertEqual(user.name, data['name'])
+        self.assertIsNone(user.group_id)
+
+    def test_get_users(self):
+        user1 = User(email='test1@test.com', password='password1', name='Test User 1')
+        user2 = User(email='test2@test.com', password='password2', name='Test User 2')
+        db.session.add(user1)
+        db.session.add(user2)
+        db.session.commit()
+
+        response = self.app.get('/users')
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json, {
+            'users': [{'email': 'test1@test.com', 'password': 'password1', 'name': 'Test User 1', 'group_id': None},
+                      {'email': 'test2@test.com', 'password': 'password2', 'name': 'Test User 2', 'group_id': None}]})
+
+    def test_get_user(self):
+        # create a user for testing
+        user = User(email='testuser@example.com', password='password', name='Test User')
+        db.session.add(user)
+        db.session.commit()
+
+        # perform GET request
+        response = self.app.get('/users/1')
+        self.assertEqual(response.status_code, 200)
+
+        # validate response
+        expected_response = {"name": "Test User", "email": "testuser@example.com", "group": None}
+        self.assertEqual(response.json, expected_response)
+
+    def test_add_user(self):
+        # perform POST request
+        data = {"email": "newuser@example.com", "password": "password", "name": "New User"}
+        response = self.app.post('/users', json=data)
+        self.assertEqual(response.status_code, 200)
+
+        # validate response
+        self.assertEqual(response.json['id'], 2)
+
+        # validate data was added to database
+        user = User.query.filter_by(email='newuser@example.com').first()
+        self.assertEqual(user.name, 'New User')
+        self.assertEqual(user.group_id, None)
+
+    def test_update_user(self):
+        # create a user for testing
+        user = User(email='testuser@example.com', password='password', name='Test User')
+        db.session.add(user)
+        db.session.commit()
+
+        # perform PUT request
+        data = {"email": "testuser@example.com", "name": "Updated Test User", "group_id": 1}
+        response = self.app.put('/users/1', json=data)
+        self.assertEqual(response.status_code, 200)
+
+        # validate response
+        self.assertEqual(response.json, {"message": "updated"})
+
+        # validate data was updated in database
+        user = User.query.filter_by(id=1).first()
+        self.assertEqual(user.name, 'Updated Test User')
+        self.assertEqual(user.group_id, 1)
+
+    def test_delete_user(self):
+        # create a user for testing
+        user = User(email='testuser@example.com', password='password', name='Test User')
+        db.session.add(user)
+        db.session.commit()
+
+        # perform DELETE request
+        response = self.app.delete('/users/1')
+        self.assertEqual(response.status_code, 200)
+
+        # validate response
+        self.assertEqual(response.json, {"message": "deleted"})
+
+        # validate data was deleted from database
+        user = User.query.filter_by(id=1).first()
+        self.assertEqual(user, None)
